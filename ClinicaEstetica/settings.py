314f9d664ccbde0e445606a945285d85851b907e
portelarommel <portelarommel@gmail.com>
2026-08-11 19:00:29 -0300

    feat: Adicionado a classe agendamento e a função realizar agendamento

--- ClinicaEstetica/settings.py
+++ ClinicaEstetica/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'ClinicaEstetica.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'TEMPLATES'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
