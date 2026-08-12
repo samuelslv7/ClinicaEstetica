5fca54f5ba70990c038e3edafecca5ea7bc83e61
Samuel Silva <samuelslv7@gmail.com>
2026-08-12 00:28:22 -0300

    feat: adiconar formulario de criação de cliente
    Closes #12

--- agendar/admin.py
+++ agendar/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Cliente
+from .models import Cliente, AgendamentoModel, HorarioModel
 
 admin.site.register(Cliente)
+admin.site.register(AgendamentoModel)
+admin.site.register(HorarioModel)
--- agendar/views.py
+++ agendar/views.py
@@ -1,9 +1,10 @@
-from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseRedirect
-from .forms import ClienteForm, HorarioForm, AgendamentoForm
+from django.shortcuts import redirect, render
+from django.http import HttpResponse, HttpResponseRedirect, HttpRequest
+from .forms import ClienteForm, AgendamentoForm, HorarioForm
 from django.urls import reverse
 
 from .models import HorarioModel, AgendamentoModel
+
 
 def index(request):
     return HttpResponse("Hello, world.")
@@ -33,22 +34,19 @@ def horario_cadastrar(request: HttpRequest):
         if formulario.is_valid():
             formulario.save()
             return redirect("horarios:home")
-    contexto = {
-        "form" : HorarioForm
-    }
-    return render(request,'horarios/cadastrarHorario.html',contexto)
+    contexto = {"form": HorarioForm}
+    return render(request, "horarios/cadastrarHorario.html", contexto)
 
-def agendamento_realizar(request:HttpRequest):
+
+def agendamento_realizar(request: HttpRequest):
     if request.method == "POST":
         formulario = AgendamentoForm(request.POST)
         if formulario.is_valid():
-            agendamento = formulario.save(commit = False )
+            agendamento = formulario.save(commit=False)
             horario = agendamento.horario
-            horario.livre = False 
+            horario.livre = False
             horario.save()
             agendamento.save()
             return redirect("agendamento:home")
-    contexto = {
-        "form" : AgendamentoForm()
-    }
-    return render(request, 'agendamento/realizarAgendamento.html',contexto)
+    contexto = {"form": AgendamentoForm()}
+    return render(request, "agendamento/realizarAgendamento.html", contexto)
